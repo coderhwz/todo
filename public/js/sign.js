2a98c3dc58e048a445677319c5a596cbd424ec65
coderhwz@home <coderhwz@gmail.com>
2017-02-18 22:01:54 +0800

    jquery 准备好时，再执行基于jquery的调用

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,3 +1,6 @@
-$(".register_nav a").click(function(){
-  $(this).addClass("active");
-})
+// jQuery准备好时，再执行基于jQuery的调用,以下的这个函数等同于 $.ready(function(){}); 调用
+$(function(){
+    $(".register_nav a").click(function(){
+        $(this).addClass("active");
+    })
+});
