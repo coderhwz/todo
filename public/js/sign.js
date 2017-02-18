ee4feebbe31179bdfb674fb3e8224af96ad1a361
黄文镇 <coderhwz@gmail.com>
2017-02-18 23:48:24 +0800

    注册页面修改

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,9 +1,12 @@
 // jQuery准备好时，再执行基于jQuery的调用,以下的这个函数等同于 $.ready(function(){}); 调用
 $(function(){
     $(".register_nav a").click(function(){
-    	var detaActive = $(this).attr('date-active')
-        $(this).addClass("active").siblings().removeClass('active');
-        $('.tab_active').animate({left:detaActive*140});
-        $('div[date-box="datebox"]').eq(detaActive).show().siblings().hide();
-    })
+    	var detaActive = $(this).attr('date-active'),
+	    	_this = $(this);
+	    	tabActive = $('.tab_active'),
+	    	dateBox = $('div[date-box="datebox"]');
+        _this.addClass("active").siblings().removeClass('active');
+        tabActive.animate({left:detaActive*140});
+        dateBox.eq(detaActive).show().siblings().hide();
+    });
 });
