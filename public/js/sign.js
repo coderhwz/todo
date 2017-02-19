cb19f459b795dc0ff5450849d6aa9e8c84d45a5c
coderhwz@home <coderhwz@gmail.com>
2017-02-19 13:53:50 +0800

    修复post问题

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,24 +1,26 @@
 // jQuery准备好时，再执行基于jQuery的调用,以下的这个函数等同于 $.ready(function(){}); 调用
 $(function(){
     $(".register_nav a").click(function(){
-    	var detaActive = $(this).attr('date-active'),
-	    	_this = $(this);
-	    	tabActive = $('.tab_active'),
-	    	dateBox = $('div[date-box="datebox"]');
+        var detaActive = $(this).attr('date-active'),
+        _this = $(this);
+        tabActive = $('.tab_active'),
+        dateBox = $('div[date-box="datebox"]');
         _this.addClass('active').siblings().removeClass('active');
         tabActive.animate({left:detaActive*140});
         dateBox.eq(detaActive).show().siblings().hide();
     });
-   $(".phone_box input[type=submit]").click(function(){
-	    var phoneNumber = $('.phone_box  .phone_number').attr('placeholder');
-	    var verificationCode = $('.phone_box  .passwod_lit').attr('placeholder');
-	    var phonePssword = $('.phone_box  input[type=password]').attr('placeholder');
-	   	$.post("/users/create",{
-	   		loginName:phoneNumber,
-	   		password:verificationCode,
-	   		repassword:phonePssword
-   	},function(date,status){
-   		alert("数据:"+date +"\状态:"+status);
-   	});
-   });
+    $(".phone_box input[type=submit]").click(function(event){
+        event.preventDefault();
+        var phoneNumber = $('.phone_box  .phone_number').attr('placeholder');
+        var verificationCode = $('.phone_box  .passwod_lit').attr('placeholder');
+        var phonePssword = $('.phone_box  input[type=password]').attr('placeholder');
+        $.post("/users/create",{
+            loginName:phoneNumber,
+            password:verificationCode,
+            repassword:phonePssword
+        },function(data,status){
+            console.log(data);
+            //alert("数据:"+data +"\状态:"+status);
+        });
+    });
 });
